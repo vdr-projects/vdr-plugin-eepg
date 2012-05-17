4ce3841972c23d31a1dfb64f068ceeb6e8d35cd9
Dimitar Petrovski <dimeptr@gmail.com>
2012-05-17 14:15:55 +0200

    removed EIT filter for NA since it should be done by epg-handler modified indentation

--- epghandler.h
+++ epghandler.h
@@ -13,27 +13,27 @@
 
 class cEEpgHandler : public cEpgHandler {
 public:
-	cEEpgHandler();
-	virtual ~cEEpgHandler();
-	virtual bool IgnoreChannel(const cChannel *Channel) { return false; }
-	virtual bool HandleEitEvent(cSchedule *Schedule, const SI::EIT::Event *EitEvent, uchar TableID, uchar Version);
-	virtual bool SetEventID(cEvent *Event, tEventID EventID);
-	virtual bool SetTitle(cEvent *Event, const char *Title);
-	virtual bool SetShortText(cEvent *Event, const char *ShortText);
-	virtual bool SetDescription(cEvent *Event, const char *Description);
-	virtual bool SetContents(cEvent *Event, uchar *Contents);
-	virtual bool SetParentalRating(cEvent *Event, int ParentalRating);
-	virtual bool SetStartTime(cEvent *Event, time_t StartTime);
-	virtual bool SetDuration(cEvent *Event, int Duration);
-	virtual bool SetVps(cEvent *Event, time_t Vps);
-	virtual bool FixEpgBugs(cEvent *Event) { return false; }
-	virtual bool HandleEvent(cEvent *Event);
-	virtual bool SortSchedule(cSchedule *Schedule);
-	virtual bool DropOutdated(cSchedule *Schedule, time_t SegmentStart, time_t SegmentEnd, uchar TableID, uchar Version);
+    cEEpgHandler();
+    virtual ~cEEpgHandler();
+    virtual bool IgnoreChannel(const cChannel *Channel) { return false; }
+    virtual bool HandleEitEvent(cSchedule *Schedule, const SI::EIT::Event *EitEvent, uchar TableID, uchar Version);
+    virtual bool SetEventID(cEvent *Event, tEventID EventID);
+    virtual bool SetTitle(cEvent *Event, const char *Title);
+    virtual bool SetShortText(cEvent *Event, const char *ShortText);
+    virtual bool SetDescription(cEvent *Event, const char *Description);
+    virtual bool SetContents(cEvent *Event, uchar *Contents);
+    virtual bool SetParentalRating(cEvent *Event, int ParentalRating);
+    virtual bool SetStartTime(cEvent *Event, time_t StartTime);
+    virtual bool SetDuration(cEvent *Event, int Duration);
+    virtual bool SetVps(cEvent *Event, time_t Vps);
+    virtual bool FixEpgBugs(cEvent *Event) { return false; }
+    virtual bool HandleEvent(cEvent *Event);
+    virtual bool SortSchedule(cSchedule *Schedule);
+    virtual bool DropOutdated(cSchedule *Schedule, time_t SegmentStart, time_t SegmentEnd, uchar TableID, uchar Version);
 
 private:
-	std::string origShortText;
-	std::string origDescription;
+    std::string origShortText;
+    std::string origDescription;
 };
 
 #endif /*APIVERSNUM > 10725*/
